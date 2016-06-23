822761efde3e3e07849d4fc68663458f4ef4dab2
lishiyi <lishiyiiii@gmail.com>
2016-06-23 01:26:32 +0000

    Async.js Completed

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    	var promise = new Promise(function(resolve, reject){
+    		resolve(value);
+    	}); 
+    	return promise; 
   },
 
   manipulateRemoteData: function(url) {
 
+       	var names = []; 	
+    	var promise = new Promise(function(resolve, reject) {
+
+	    	$.getJSON(url, function(data){
+	    		$.each(data["people"], function (key, value) {
+	    			names.push(value);
+	    		});
+	    		
+	    	});
+	    	resolve(names.sort());
+    	});
+
+    	return promise; 
   }
 };
